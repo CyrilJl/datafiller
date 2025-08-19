8645e9150ff11d7c3d30deeb75c1c0caa9f5b249
Cyril <joly.cyril7@gmail.com>
2025-08-19 16:45:42 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,36 +1,22 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = "datafiller"
 author = "Cyril Joly"
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
-    "sphinxawesome_theme.highlighting",
+    "sphinxawesome_theme",
     "myst_parser",
     "sphinx_togglebutton",
 ]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_title = project
 html_theme = "sphinxawesome_theme"
